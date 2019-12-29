f6263b888cdc9dc41ab4a4054e20af653335ade8
yaohua.xu <yaohua.xu@zongmutech.com>
2019-12-29 13:38:28 +0800

    complete Processor::Utilization()

--- src/processor.cpp
+++ src/processor.cpp
@@ -9,23 +9,30 @@ using std::stof;
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
     std::vector<std::string> cpu_utilization = LinuxParser::CpuUtilization();
+    static std::vector<std::string> cpu_utilization_last = cpu_utilization;
     // std::cout << "cpu_utilization[kUser_]" << cpu_utilization[LinuxParser::kUser_] << std::endl;
     // std::cout << "cpu_utilization[kNice_]" << cpu_utilization[LinuxParser::kNice_] << std::endl;
     // std::cout << "cpu_utilization[kSystem_]" << cpu_utilization[LinuxParser::kSystem_] << std::endl;
     // std::cout << "cpu_utilization[kIdle_]" << cpu_utilization[LinuxParser::kIdle_] << std::endl;
-    static std::vector<std::string> cpu_utilization_last = cpu_utilization;
 
-    const auto idles_waits = cpu_utilization[LinuxParser::kIdle_] + cpu_utilization[LinuxParser::kIOwait_];
-    const auto prev_idles_waits = cpu_utilization_last[LinuxParser::kIdle_] + cpu_utilization_last[LinuxParser::kIOwait_];
+    const auto Idle = stof(cpu_utilization[LinuxParser::kIdle_]) + stof(cpu_utilization[LinuxParser::kIOwait_]);
+    const auto PrevIdle = stof(cpu_utilization_last[LinuxParser::kIdle_]) + stof(cpu_utilization_last[LinuxParser::kIOwait_]);
 
-    const auto nonidle = cpu_utilization[LinuxParser::kUser_] + cpu_utilization[LinuxParser::kNice_] + cpu_utilization[LinuxParser::kSystem_] + cpu_utilization[LinuxParser::kIRQ_] + cpu_utilization[LinuxParser::kSoftIRQ_] + cpu_utilization[LinuxParser::kSteal_];
-    const auto prev_nonidle = cpu_utilization_last[LinuxParser::kUser_] + cpu_utilization_last[LinuxParser::kNice_] + cpu_utilization_last[LinuxParser::kSystem_] + cpu_utilization_last[LinuxParser::kIRQ_] + cpu_utilization_last[LinuxParser::kSoftIRQ_] + cpu_utilization_last[LinuxParser::kSteal_];
+    const auto nonidle = stof(cpu_utilization[LinuxParser::kUser_]) + stof(cpu_utilization[LinuxParser::kNice_]) + stof(cpu_utilization[LinuxParser::kSystem_]) +
+                         stof(cpu_utilization[LinuxParser::kIRQ_]) + stof(cpu_utilization[LinuxParser::kSoftIRQ_]) + stof(cpu_utilization[LinuxParser::kSteal_]);
+    const auto prev_nonidle = stof(cpu_utilization_last[LinuxParser::kUser_]) + stof(cpu_utilization_last[LinuxParser::kNice_]) + stof(cpu_utilization_last[LinuxParser::kSystem_]) +
+                              stof(cpu_utilization_last[LinuxParser::kIRQ_]) + stof(cpu_utilization_last[LinuxParser::kSoftIRQ_]) + stof(cpu_utilization_last[LinuxParser::kSteal_]);
 
-    const auto total = idles_waits + nonidle;
-    const auto prev_total = prev_idles_waits + prev_nonidle;
+    const auto total = Idle + nonidle;
+    const auto prev_total = PrevIdle + prev_nonidle;
 
-    const auto totald = total + prev_total;
-    const auto idled = idles_waits + prev_idles_waits;
+    auto totald = total - prev_total;
+    totald = totald < 0.0001f ? 0.0001f : totald;
+    const auto idled = Idle - PrevIdle;
+    const float precent = (totald - idled) / totald;
+    // std::cout << "totald: " << totald << ", idled: " << idled << ", totald - idled: " << totald - idled << ", a: " << a <<std::endl;
 
-    return (stof(totald) - stof(idled)) / stof(totald);
+    cpu_utilization_last = cpu_utilization;
+
+    return precent;
 }
